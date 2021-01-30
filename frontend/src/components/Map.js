0821aaa816a11e2e8b08bb492f597047c7412eb4
Robert Bragg <superbleh100@gmail.com>
2021-01-30 17:27:54 -0500

    adding Marker Clusters to map

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'react-leaflet-markercluster/dist/styles.min.css';
+import MarkerClusterGroup from 'react-leaflet-markercluster'
+import { data } from "../data/markers"
 
 const wrapper = {
     height: '100vh',
@@ -8,23 +11,30 @@ const wrapper = {
   }
 
 class Map extends Component {
-    state = {
-        lat: 37.7759,
-        lng: -122.4194,
-        zoom: 13,
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            lat: 37.7759,
+            lng: -122.4194,
+            zoom: 13,
+            markers: data
+        };
+    }
+
+
     render() {
         return (
-            <MapContainer style={wrapper} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+            <MapContainer style={wrapper} center={[40.4406, -79.9959]} zoom={14} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                />  
+                <MarkerClusterGroup>
+                    {
+                    this.state.markers.map((marker) => {
+                        return (<Marker position={[marker.lat, marker.long]} />);
+                    })}
+                </MarkerClusterGroup>
             </MapContainer>
         )
     }
